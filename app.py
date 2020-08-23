a2d5e9b0f6881a40dd7d086323f795e6e38f8033
spencer <soorte214@gmail.com>
2020-08-23 21:49:31 +0900

    확진자 동선 날짜별 저장

--- app.py
+++ app.py
@@ -9,8 +9,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # 플라스크 앱 실행
 app = Flask(__name__)
 
-# 임시 카운트
-count = 1
+
 
 # 라우팅
 @app.route('/upload')
@@ -29,20 +28,32 @@ def upload_file():
 @app.route('/receiveJson',methods=['POST'])
 def receive_json():
     response = request.get_json()
+    print(response)
+    day = response.get('day')
+    number = response.get('number')
+    geojson = response.get('geojson')
 
     # k와 같은 폴더가 없으면 생성
-    if not os.path.isdir(os.path.join(BASE_DIR,'data')):
-        os.makedirs(os.path.join(BASE_DIR,'data'))
-    dir_name = os.path.join(BASE_DIR,'data')
-    # print(dir_name)
+    if not os.path.isdir(os.path.join(BASE_DIR,'static','data',day)):
+        os.makedirs(os.path.join(BASE_DIR,'static','data',day))
+    dir_name = os.path.join(BASE_DIR,'static','data',day)
+    print(dir_name)
 
     # json 파일로 저장    
-    global count
-    filename = '{}.json'.format(count)
+    filename = '{}.json'.format(number)
     with open(os.path.join(dir_name,filename), 'w', encoding='UTF-8') as fp:
-            json.dump(response, fp, ensure_ascii=False, indent='\t')
-    count += 1
-    return json.dumps(response)
+            json.dump(geojson, fp, ensure_ascii=False, indent='\t')
+
+    # list.json 생성
+    file_list = os.listdir(dir_name)
+    file_list.remove('list.json')
+    with open(os.path.join(dir_name,'list.json'), 'w', encoding='UTF-8') as fp:
+            json.dump(file_list, fp, ensure_ascii=False, indent='\t')
+    
+    
+
+
+    return json.dumps(geojson)
 
 @app.route('/draw_test')
 def draw_test():
